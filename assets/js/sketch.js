48228f55518825f133346855668772e3a8a79bac
SoyaNyan <nyan@soya.moe>
2021-07-19 18:38:21 +0900

    model path edited

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -2,7 +2,7 @@
 // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
 // the link to your model provided by Teachable Machine export panel
-const URL = "./my_model/";
+const URL = "/data/";
 
 let model, webcam, labelContainer, maxPredictions;
 
