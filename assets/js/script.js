4f4da33e1d7b426f4479ebfdc638605c7924163a
SoyaNyan <nyan@soya.moe>
2021-07-19 18:12:44 +0900

    Gesture algorithm updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,122 @@
-var handsDiv = document.getElementById("hands");
-var fingersDiv = document.getElementById("fingers");
+const className = ["play", "pause", "prev", "next", "none"];
+const gestureRule = {
+	play: {
+		className: "play",
+		hands: 1,
+		fingers: 4,
+	},
+	pause: {
+		className: "pause",
+		hands: 1,
+		fingers: 0,
+	},
+	prev: {
+		className: "prev",
+		hands: 1,
+		fingers: 2,
+	},
+	next: {
+		className: "next",
+		hands: 1,
+		fingers: 3,
+	},
+	none: {
+		className: "none",
+		hands: 0,
+		fingers: 0,
+	},
+};
+const gestureQueueSize = 3;
+const gestureSensitivity = 2;
+let gestureArray = [];
 
-var handsDesc = document.getElementById("description-hands");
-var fingersDesc = document.getElementById("description-fingers");
+let leapObj = {
+	hands: 0,
+	fingers: 0,
+};
+
+let modelPredict = {
+	className: "none",
+	probability: 0,
+};
 
 Leap.loop(function (obj) {
-    var hands = obj.hands.length;
-    var fingers = obj.pointables.length;
+	leapObj.hands = obj.hands.length;
+	leapObj.fingers = obj.pointables.length;
 
-    handsDiv.innerHTML = hands;
-    fingersDiv.innerHTML = fingers;
-
-    var a = hands == 1 ? "hand" : "hands";
-    var b = fingers == 1 ? "finger" : "fingers";
-
-    handsDesc.innerHTML = a;
-    fingersDesc.innerHTML = b;
-
-    document.title = [hands, a, fingers, b].join(" ");
+	console.log(leapObj);
 });
+
+$(document).ready(function () {
+	gestureInit();
+	//init();
+
+	// timer
+	window.setInterval(() => {
+		switch (checkGesture()) {
+			case "play":
+				$("#play-pause-button").click();
+				break;
+			case "pause":
+				$("#play-pause-button").click();
+				break;
+			case "prev":
+				$("#play-previous").click();
+				break;
+			case "next":
+				$("#play-next").click();
+				break;
+			default:
+		}
+	}, 1000);
+});
+
+const checkGesture = () => {
+	pushGesture();
+
+	let gestureClass = "none";
+	if (gestureArray.length >= gestureQueueSize) {
+		gestureClass = findMostFreqElement(gestureArray);
+		gestureInit();
+	}
+
+	return gestureClass;
+};
+
+const findMostFreqElement = (arr) => {
+	return arr
+		.sort((a, b) => {
+			return arr.filter((e) => e === a).length - arr.filter((e) => e === b).length;
+		})
+		.pop();
+};
+
+const checkGestureRule = (l, m) => {
+	if (
+		l.hands === gestureRule[m.className].hands &&
+		l.fingers === gestureRule[m.className].fingers
+	) {
+		return m.className;
+	} else {
+		return "none";
+	}
+};
+
+const gestureInit = () => {
+	gestureArray = [];
+};
+
+const pushGesture = () => {
+	if (gestureArray.length < gestureQueueSize) {
+		gestureArray.push(checkGestureRule(leapObj, modelPredict));
+	} else {
+		shiftGesture();
+		gestureArray.push(checkGestureRule(leapObj, modelPredict));
+	}
+
+	return gestureArray.length;
+};
+
+const shiftGesture = () => {
+	return gestureArray.shift();
+};
